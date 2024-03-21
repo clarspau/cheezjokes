c79fec73fefc212731fabf8f2da444255e2b36d8
clarspau <125636195+clarspau@users.noreply.github.com>
2024-03-20 22:15:44 -0700

    updated

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 ```
 npm install && npm start
 ```
-# cheezjokes
+
+# CheezJokes
--- src/Joke.js
+++ src/Joke.js
@@ -1,30 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Joke.css";
 
-/** A single joke, along with vote up/down buttons. */
+/** Presentational joke component with voting buttons */
 
-class Joke extends Component {
-  render() {
-    const { id, vote, votes, text } = this.props;
+function Joke({ vote, votes, text, id }) {
+  function upVote(evt) { vote(id, +1); }
+  function downVote(evt) { vote(id, -1); }
 
-    return (
-      <div className="Joke">
-        <div className="Joke-votearea">
-          <button onClick={evt => vote(id, +1)}>
-            <i className="fas fa-thumbs-up" />
-          </button>
+  return (
+    <div className="Joke">
+      <div className="Joke-votearea">
+        <button onClick={upVote}>
+          <i className="fas fa-thumbs-up" />
+        </button>
 
-          <button onClick={evt => vote(id, -1)}>
-            <i className="fas fa-thumbs-down" />
-          </button>
+        <button onClick={downVote}>
+          <i className="fas fa-thumbs-down" />
+        </button>
 
-          {votes}
-        </div>
-
-        <div className="Joke-text">{text}</div>
+        {votes}
       </div>
-    );
-  }
+
+      <div className="Joke-text">{text}</div>
+    </div>
+  );
 }
 
 export default Joke;
